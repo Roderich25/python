ef2dd195e484f3b500bce153bf1a124f18b283a0
Roderich25 <rodrigoavilasolis@gmail.com>
2019-05-28 19:38:32 -0500

    adding opp.py

---
+++ opp.py
@@ -0,0 +1,28 @@
+class Dog:
+    
+    def __init__ (self, name):
+        self.name=name
+    
+    def __init__ (self, name, age):
+        self.name = name
+        self.age = age
+    
+    def setAge(self, age):
+        self.age += age
+    
+    def to_String(self):
+        print(f"This dog is called {self.name} and it's {self.age} years old.")
+
+def main():
+    d1 = Dog("Audrey", 6)
+    d2 = Dog(name="Pelusa", age=2)
+    d3 = Dog(name="Marshall", age=4)
+    d2.setAge(4)
+    d3.setAge(2)
+    d1.to_String()
+    d2.to_String()
+    d3.to_String()
+
+if __name__ == "__main__":
+    main()
+
