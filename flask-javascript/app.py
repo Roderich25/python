d45c2bb974767cd9a99f800f322606976516336c
Roderich25 <rodrigoavilasolis@gmail.com>
2019-06-06 10:50:42 -0500

    added javascript folder and modified flask-javascript one

---
+++ flask-javascript/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+texts = ["¡Hola mundo!", "Hello world!", "Bonjour le monde!"]
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/Español")
+def Español():
+    return texts[0]
+
+
+@app.route("/English")
+def English():
+    return texts[1]
+
+
+@app.route("/Français")
+def Français():
+    return texts[2]
+
+
+if __name__ == '__main__':
+    app.run(port=9130, debug=True)
