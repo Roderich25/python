48ae671fc13119eced07833010c4f36a1930c725
Roderich25 <rodrigoavilasolis@gmail.com>
2019-06-28 10:35:52 -0500

    add last changes to django-blog

--- django-blog/blog/views.py
+++ django-blog/blog/views.py
@@ -1,33 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Rodrigo',
-        'title': 'My First Post',
-        'content': 'Something goes here...',
-        'date': 'Jun 21, 2019'
-    },
-    {
-        'author': 'Pablo Rodrigo',
-        'title': 'My Second Post',
-        'content': 'Something goes here...',
-        'date': 'Jun 22, 2019'
-    },
-    {
-        'author': 'Pablo',
-        'title': 'My Third Post',
-        'content': 'Something goes here...',
-        'date': 'Jun 23, 2019'
-    }
-]
+from blog.models import Post
 
 
 # Create your views here.
 
 def home(request):
+
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context=context)
 
