7b05e65cf6960584cd7a210cbd8d70f6ffdf2567
Roderich25 <rodrigoavilasolis@gmail.com>
2019-07-04 21:08:33 -0500

    matplotlib barchart example

---
+++ python-scripts/matplotlib_barchart.py
@@ -0,0 +1,32 @@
+# import csv
+import pandas
+from collections import Counter
+from matplotlib import pyplot
+
+pyplot.style.use('fivethirtyeight')
+language_counter = Counter()
+
+# with open('data.csv') as csv_file:
+#    csv_reader = csv.DictReader(csv_file)
+# for row in csv_reader:
+# language_counter.update(row['LanguagesWorkedWith'].split(';'))
+
+data = pandas.read_csv('data.csv')
+ids = data['Responder_id']
+lang_responses = data['LanguagesWorkedWith']
+for response in lang_responses:
+    language_counter.update(response.split(';'))
+
+languages = []
+popularity = []
+for item in language_counter.most_common(15):
+    languages.append(item[0])
+    popularity.append(item[1])
+languages.reverse()
+popularity.reverse()
+
+pyplot.barh(languages, popularity)
+pyplot.title('Most popular programming languages')
+pyplot.xlabel("People who use it")
+pyplot.tight_layout()
+pyplot.show()
