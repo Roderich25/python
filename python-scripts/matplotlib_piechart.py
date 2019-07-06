f8727a58863bbf506b79912437f981e380efc8b6
Roderich25 <rodrigoavilasolis@gmail.com>
2019-07-05 23:10:54 -0500

    adding matplot piechart script

---
+++ python-scripts/matplotlib_piechart.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+
+# plt.style.use('fivethirtyeight')
+plt.xkcd()
+
+# Language Popularity
+slices = [59219, 55466, 47544, 36443, 35917]
+labels = ['JavaScript', 'HTML/CSS', 'SQL', 'Python', 'Java']
+explode = [0, 0, 0, 0.1, 0]
+colors = ['#008fd5', '#fc4f30', '#e5ae37', '#6d904f', '#4c4c4c']
+
+plt.pie(slices, labels=labels, explode=explode, shadow=True, startangle=90,
+        autopct='%1.1f%%', colors=colors, wedgeprops={'edgecolor': 'black'})
+
+plt.title('Programming Languages Pie Chart')
+plt.tight_layout()
+plt.show()
