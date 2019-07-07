b54c08a812b5626a3db935ebadbc3f5d34bcd068
Roderich25 <rodrigoavilasolis@gmail.com>
2019-07-07 14:55:15 -0500

    adding matplotlib fill between code

---
+++ python-scripts/matplotlib_fillbetween.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+data = pd.read_csv('data_fillbetween.csv')
+ages = data['Age']
+dev_salaries = data['All_Devs']
+py_salaries = data['Python']
+js_salaries = data['JavaScript']
+
+plt.plot(ages, dev_salaries, color='#444444', linestyle='--', label='All Devs')
+plt.plot(ages, py_salaries, label='Python')
+
+overall_median = 57_287
+# plt.fill_between(ages, py_salaries, alpha=0.25) # Default third argument is zero
+# plt.fill_between(ages, py_salaries, overall_median, alpha=0.25) # Filling up and down the oveall median
+plt.fill_between(ages, py_salaries, overall_median, where=(py_salaries > overall_median),
+                 interpolate=True, alpha=0.25, label='Above Overall Median')
+plt.fill_between(ages, py_salaries, overall_median, where=(py_salaries < overall_median),
+                 interpolate=True, color='red', alpha=0.25, label='Below Overall Median')
+plt.xlabel('Ages')
+plt.ylabel('USD')
+plt.title('Median Salary by Age')
+
+plt.legend()
+plt.tight_layout()
+plt.show()
